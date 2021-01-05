5b5e08087780b601de34426eb585a5473980c23b
Koenraad Tubbax <koenraad@tubbax.be>
2021-01-05 20:26:40 +0100

    Add project files.

---
+++ 5h6vb02/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDebug = New System.Windows.Forms.Button()
+        Me.btnTekstvak = New System.Windows.Forms.Button()
+        Me.btnNegatief = New System.Windows.Forms.Button()
+        Me.btnLussen = New System.Windows.Forms.Button()
+        Me.btnOneindige = New System.Windows.Forms.Button()
+        Me.btnSluiten = New System.Windows.Forms.Button()
+        Me.txtTekst = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnDebug
+        '
+        Me.btnDebug.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDebug.Location = New System.Drawing.Point(32, 12)
+        Me.btnDebug.Name = "btnDebug"
+        Me.btnDebug.Size = New System.Drawing.Size(376, 121)
+        Me.btnDebug.TabIndex = 0
+        Me.btnDebug.Text = "Uitvoer in immediate venster"
+        Me.btnDebug.UseVisualStyleBackColor = True
+        '
+        'btnTekstvak
+        '
+        Me.btnTekstvak.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTekstvak.Location = New System.Drawing.Point(32, 139)
+        Me.btnTekstvak.Name = "btnTekstvak"
+        Me.btnTekstvak.Size = New System.Drawing.Size(376, 121)
+        Me.btnTekstvak.TabIndex = 1
+        Me.btnTekstvak.Text = "Uitvoer in tekstvak"
+        Me.btnTekstvak.UseVisualStyleBackColor = True
+        '
+        'btnNegatief
+        '
+        Me.btnNegatief.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNegatief.Location = New System.Drawing.Point(32, 265)
+        Me.btnNegatief.Name = "btnNegatief"
+        Me.btnNegatief.Size = New System.Drawing.Size(376, 121)
+        Me.btnNegatief.TabIndex = 2
+        Me.btnNegatief.Text = "Negatieve stapgrootte"
+        Me.btnNegatief.UseVisualStyleBackColor = True
+        '
+        'btnLussen
+        '
+        Me.btnLussen.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLussen.Location = New System.Drawing.Point(32, 383)
+        Me.btnLussen.Name = "btnLussen"
+        Me.btnLussen.Size = New System.Drawing.Size(376, 121)
+        Me.btnLussen.TabIndex = 3
+        Me.btnLussen.Text = "Geneste lussen"
+        Me.btnLussen.UseVisualStyleBackColor = True
+        '
+        'btnOneindige
+        '
+        Me.btnOneindige.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOneindige.Location = New System.Drawing.Point(32, 499)
+        Me.btnOneindige.Name = "btnOneindige"
+        Me.btnOneindige.Size = New System.Drawing.Size(376, 121)
+        Me.btnOneindige.TabIndex = 4
+        Me.btnOneindige.Text = "Oneindige lus"
+        Me.btnOneindige.UseVisualStyleBackColor = True
+        '
+        'btnSluiten
+        '
+        Me.btnSluiten.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSluiten.Location = New System.Drawing.Point(32, 617)
+        Me.btnSluiten.Name = "btnSluiten"
+        Me.btnSluiten.Size = New System.Drawing.Size(376, 121)
+        Me.btnSluiten.TabIndex = 5
+        Me.btnSluiten.Text = "Sluiten"
+        Me.btnSluiten.UseVisualStyleBackColor = True
+        '
+        'txtTekst
+        '
+        Me.txtTekst.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTekst.Location = New System.Drawing.Point(436, 24)
+        Me.txtTekst.Multiline = True
+        Me.txtTekst.Name = "txtTekst"
+        Me.txtTekst.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtTekst.Size = New System.Drawing.Size(575, 684)
+        Me.txtTekst.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1085, 740)
+        Me.Controls.Add(Me.txtTekst)
+        Me.Controls.Add(Me.btnSluiten)
+        Me.Controls.Add(Me.btnOneindige)
+        Me.Controls.Add(Me.btnLussen)
+        Me.Controls.Add(Me.btnNegatief)
+        Me.Controls.Add(Me.btnTekstvak)
+        Me.Controls.Add(Me.btnDebug)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDebug As Button
+    Friend WithEvents btnTekstvak As Button
+    Friend WithEvents btnNegatief As Button
+    Friend WithEvents btnLussen As Button
+    Friend WithEvents btnOneindige As Button
+    Friend WithEvents btnSluiten As Button
+    Friend WithEvents txtTekst As TextBox
+End Class
---
+++ 5h6vb02/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub btnSluiten_Click(sender As Object, e As EventArgs) Handles btnSluiten.Click
+        End
+    End Sub
+
+    Private Sub btnDebug_Click(sender As Object, e As EventArgs) Handles btnDebug.Click
+
+        For a As Single = 1 To 3 Step 0.5
+            Debug.WriteLine(a)
+        Next
+
+    End Sub
+
+    Private Sub btnTekstvak_Click(sender As Object, e As EventArgs) Handles btnTekstvak.Click
+        For b As Double = 1 To 5 Step 0.2
+            txtTekst.Text = txtTekst.Text & b.ToString & vbCrLf
+
+        Next
+    End Sub
+
+    Private Sub btnNegatief_Click(sender As Object, e As EventArgs) Handles btnNegatief.Click
+        For c As Integer = 100 To 1 Step -1
+            txtTekst.Text = txtTekst.Text & c.ToString & vbCrLf
+        Next
+    End Sub
+
+    Private Sub btnLussen_Click(sender As Object, e As EventArgs) Handles btnLussen.Click
+        Dim a, b As Integer
+        For a = 1 To 5
+            For b = 1 To 10
+                txtTekst.Text = txtTekst.Text & a.ToString & " - " & b.ToString & vbCrLf
+            Next
+        Next
+    End Sub
+
+    Private Sub btnOneindige_Click(sender As Object, e As EventArgs) Handles btnOneindige.Click
+        Dim a, b As Integer
+        For a = 1 To 5
+            For b = 1 To 10
+                txtTekst.Text = txtTekst.Text & a.ToString & " - " & b.ToString & vbCrLf
+                b = 1
+            Next
+        Next
+    End Sub
+End Class
